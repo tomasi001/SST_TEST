4eb8dc430e140660ca35bc0066cdfb6b8e017329
tomasi <thomas.shields@sovtech.com>
2023-05-25 09:07:10 +0200

    Securing the API

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 export default {
   config(_input) {
@@ -9,7 +10,8 @@ export default {
       region: "us-east-1",
     };
   },
+
   stacks(app) {
-    app.stack(StorageStack).stack(ApiStack);
+    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
   },
 } satisfies SSTConfig;
